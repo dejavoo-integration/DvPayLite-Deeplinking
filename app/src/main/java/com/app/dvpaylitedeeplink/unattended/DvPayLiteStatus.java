7de9060c4c309d5e479d10bc27c7db7974894a05
BHASKAR NAVULURI <bvenkata@denovosystem.com>
2026-04-22 17:59:31 +0530

    DPAY-13367 added Notification

--- app/src/main/java/com/app/dvpaylitedeeplink/unattended/DvPayLiteStatus.java
+++ app/src/main/java/com/app/dvpaylitedeeplink/unattended/DvPayLiteStatus.java
@@ -78,7 +78,7 @@ public class DvPayLiteStatus {
                 }
             }
 
-            handler.postDelayed(this, 30000);
+            handler.postDelayed(this, 60000);
         }
     };
 
